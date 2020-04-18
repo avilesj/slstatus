899cfe58c1a4daf2db86fd9f3e04a2655021c1b8
Jose Aviles <jlapmail@gmail.com>
2020-04-17 22:50:05 -0400

    Update

--- config.h
+++ config.h
@@ -63,11 +63,12 @@ static const char unknown_str[] = "n/a";
  */
 static const struct arg args[] = {
 	/* function format          argument */
-        { netspeed_rx, "  %s/",           "enp2s0" },
-        { netspeed_tx, " %s ",           "enp2s0" },
+        { wifi_essid, "\uf1eb %s ",           "wlp4s0" },
 	{ cpu_perc, " \uf085 %s%% ",           NULL },
+	{ run_command, " %s ", "/bin/bash $HOME/.config/scripts/statusbar/temperature.sh"  },
 	{ ram_perc, " \uf2db %s%% ",           NULL },
 	{ swap_perc, "  %s%% ",           NULL },
+	{ run_command, " %s ", "/bin/bash $HOME/.config/scripts/statusbar/battery.sh"  },
 	{ run_command, "  %s%% ", "/bin/sh -c \"amixer get Master | tail -n1 | grep -Po '\\[\\K[^%]*' | head -n1\"" },
 	{ datetime, " \uf017 %s",           "%d %b (%a) %r" },
 };
